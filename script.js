236364912517c29b3ed642596828990cadefdaf8
Orizonaru <orizonaru@mail.ru>
2023-09-28 19:31:43 +0400

    TDE v1.1

--- script.js
+++ script.js
@@ -1,31 +1,53 @@
 const submitButton = document.getElementById("submit");
-let firstDigitField = document.getElementById("firstNumber");
-let secondDigitField = document.getElementById("secondNumber");
+const firstDigitField = document.getElementById("firstNumber");
+const secondDigitField = document.getElementById("secondNumber");
 const input = document.getElementById('counterInput')
+let time = 0
+
+function timerOn () {
+  setInterval(() => 
+    time+=1, 
+    1000);
+}
+
+function timerOff () {
+  clearInterval(timerOn)
+}
 
 function randomDigit(max) {
   return Math.floor((Math.random() * max)+100);
 }
-for(let i=1; i<2; i++) {
-  let firstDigit = randomDigit(899);
-  let secondDigit = randomDigit(899);
-  firstDigitField.textContent = firstDigit;
-  secondDigitField.textContent = secondDigit;
-}
 
-console.log(input.value)
+let firstDigitRoot = randomDigit(899);
+let secondDigitRoot = randomDigit(899);
+firstDigitField.textContent = firstDigitRoot;
+secondDigitField.textContent = secondDigitRoot;
 
+console.log(firstDigitRoot+secondDigitRoot)
 
-console.log(firstDigit+secondDigit)
-
+timerOn()
 input.addEventListener('keyup', (e) => {
   if(e.key === "Enter") {
-    if(firstDigit + secondDigit == input.value) {
+    console.log('first if is on')
+    console.log(firstDigitRoot+secondDigitRoot)
+    if(firstDigitRoot + secondDigitRoot == input.value) {
+      console.log('second if is on')
       let firstDigit = randomDigit(899);
       let secondDigit = randomDigit(899);
       console.log(firstDigit, secondDigit)
       firstDigitField.textContent = firstDigit;
-      secondDigitField.textContent = secondDigit;}
+      secondDigitField.textContent = secondDigit;
+      firstDigitRoot = firstDigit;
+      secondDigitRoot = secondDigit;
+      console.log(firstDigitRoot+secondDigitRoot)
+      input.value=''
+      input.classList.remove("incorrect")
+      timerOff();
+      timerOn()
+    }
+    else {    
+      input.classList.add("incorrect")
+    }
   }
 })
 

78da994f12780b5b0f41e608471812b5c2a3018b
Orizonaru <146365147+Orizonaru@users.noreply.github.com>
2023-09-30 00:06:17 +0400

    TDE v1.2
    > Added timer
    > Fixed mobile keyboard type
    > Optimization

--- script.js
+++ script.js
@@ -2,39 +2,36 @@ const submitButton = document.getElementById("submit");
 const firstDigitField = document.getElementById("firstNumber");
 const secondDigitField = document.getElementById("secondNumber");
 const input = document.getElementById('counterInput')
-let time = 0
-
-function timerOn () {
-  setInterval(() => 
-    time+=1, 
-    1000);
-}
-
-function timerOff () {
-  clearInterval(timerOn)
-}
 
 function randomDigit(max) {
   return Math.floor((Math.random() * max)+100);
+}
+
+const timerField = document.getElementById('timerField')
+let time = 0
+function timerOn () {
+  setInterval(() => 
+  (time+=1),
+  1000);  
 }
 
 let firstDigitRoot = randomDigit(899);
 let secondDigitRoot = randomDigit(899);
 firstDigitField.textContent = firstDigitRoot;
 secondDigitField.textContent = secondDigitRoot;
-
 console.log(firstDigitRoot+secondDigitRoot)
 
 timerOn()
+function timeExposer () {
+  timerField.textContent = time
+}
+setInterval(timeExposer, 1000)
+
 input.addEventListener('keyup', (e) => {
   if(e.key === "Enter") {
-    console.log('first if is on')
-    console.log(firstDigitRoot+secondDigitRoot)
     if(firstDigitRoot + secondDigitRoot == input.value) {
-      console.log('second if is on')
       let firstDigit = randomDigit(899);
       let secondDigit = randomDigit(899);
-      console.log(firstDigit, secondDigit)
       firstDigitField.textContent = firstDigit;
       secondDigitField.textContent = secondDigit;
       firstDigitRoot = firstDigit;
@@ -42,8 +39,7 @@ input.addEventListener('keyup', (e) => {
       console.log(firstDigitRoot+secondDigitRoot)
       input.value=''
       input.classList.remove("incorrect")
-      timerOff();
-      timerOn()
+      time=0
     }
     else {    
       input.classList.add("incorrect")

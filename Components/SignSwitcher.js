dd4280b38d1912ef962c546620903a40ad49e2c8
Orizonaru <orizonaru@mail.ru>
2023-11-08 13:41:44 +0300

    Sign switcher (dev) update
    > Sign swither (dev) updated

--- Components/SignSwitcher.js
+++ Components/SignSwitcher.js
@@ -1,5 +1,26 @@
 import { LitElement, html, css } from "https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js";
 
 export class SignSwitcher extends LitElement {
+    static properties = {
+        startFlag: Boolean,
+        sign: String
+    }
+
+    static styles = css`
     
+    `
+
+    connectedCallback() {
+        super.connectedCallback()
+        this.sign = ''
+    }
+
+    render() {
+        return html`
+        
+        
+        `
+    }
 }
+
+customElements.define('sign-switcher', SignSwitcher)

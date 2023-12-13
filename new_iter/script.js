524afa9700355ea9f9b508fd92a329b20a039598
Orizonaru <orizonaru@mail.ru>
2023-12-13 11:18:45 +0300

    13.12.2023 (3)

--- new_iter/script.js
+++ new_iter/script.js
@@ -1,13 +1,12 @@
-const inner = document.getElementsByClassName('outer')
+const outer = document.getElementsByClassName('outer')
+const inner = document.getElementsByClassName('inner')
 const grid = document.getElementsByClassName('main-grid')
 const main = document.getElementsByTagName('main')
 function switcher() {
-    console.log(typeof(inner[0]), inner[1])
-    inner[1].classList.toggle('started1')
-    inner[2].classList.toggle('started1')
-    inner[3].classList.toggle('started1')
-    inner[4].classList.toggle('started1')
-    
+    for (let i = 1; i < 5; i++) {
+        inner[i].classList.toggle('started1')
+        outer[i].classList.toggle('started1')
+    }
     grid[0].classList.toggle('started')
 
 }

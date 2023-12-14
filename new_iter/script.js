e5f33bf69ba6a43f206e5c35c077472ee4eb635a
Orizonaru <orizonaru@mail.ru>
2023-12-14 13:42:20 +0300

    14.12.2023

--- new_iter/script.js
+++ new_iter/script.js
@@ -3,8 +3,9 @@ const inner = document.getElementsByClassName('inner')
 const grid = document.getElementsByClassName('main-grid')
 const main = document.getElementsByTagName('main')
 function switcher() {
+    
     for (let i = 1; i < 5; i++) {
-        inner[i].classList.toggle('started1')
+        // inner[i].classList.toggle('started1')
         outer[i].classList.toggle('started1')
     }
     grid[0].classList.toggle('started')
